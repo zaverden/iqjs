3bacade9d8152490f06b23e29269cd36483cde44
Denis Zavershinskiy <zaverden@gmail.com>
2018-09-17 23:11:13 +0700

    fix queries for iterable source

--- tests/query/select.tests.js
+++ tests/query/select.tests.js
@@ -6,4 +6,24 @@ describe('select', function () {
     const iterator = select(gen(2), i => i * 2);
     expectIterator(iterator, [2, 4]);
   });
+
+  it('should convert Array values', function () {
+    const iterator = select([1, 2], i => i * 2);
+    expectIterator(iterator, [2, 4]);
+  });
+
+  it('should convert Array iterator values', function () {
+    const iterator = select([1, 2][Symbol.iterator](), i => i * 2);
+    expectIterator(iterator, [2, 4]);
+  });
+
+  it('should convert Set values', function () {
+    const iterator = select(new Set([1, 2]), i => i * 2);
+    expectIterator(iterator, [2, 4]);
+  });
+
+  it('should convert Set.values() values', function () {
+    const iterator = select(new Set([1, 2]).values(), i => i * 2);
+    expectIterator(iterator, [2, 4]);
+  });
 });
